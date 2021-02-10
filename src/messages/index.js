422d9a333a4c302cf25b4358e3f2696a83f54898
Regales <66663195+regales-0428@users.noreply.github.com>
2021-02-10 15:20:14 +0800

    Update index.js
    spelling error with archives

--- src/messages/index.js
+++ src/messages/index.js
@@ -1,9 +1,9 @@
 const messages = {
   help: 'bot is bound to ! \n\nSpecify a mode for getting your SCP `random` or an SCP number\n\nSpecify an output mode at the back of your command, either  `message` , `text`, or `audio`\n\nShorthand command `!s mode|number` will send messages by default\n\nType `!scp suggest` to get suggestions and featured scps from the wiki\n\nType !class random | className | list to get SCP classifications! ',
   ready: [
-    'yes sir, retrieving archieves from ████████',
+    'yes sir, retrieving archives from ████████',
     'SCP bot activated, searching for top secret files stored in site-████',
-    'Anything for you commander, downloading files at ip ████████'
+    'Anything for you commander, downloading files at Ip ████████'
   ]
 }
 
