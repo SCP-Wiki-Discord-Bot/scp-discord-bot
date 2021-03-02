5150d509a3b6fc0e7a8d7aa2bcecba09ec8bc281
Ronald545 <spikyron@gmail.com>
2021-03-02 08:59:46 +0800

    updated help page

--- src/messages/index.js
+++ src/messages/index.js
@@ -1,5 +1,8 @@
 const messages = {
   help: `
+  =========
+  BASICS
+  =========
   - provides the option to send SCPs in the form of \`text files\`, \`audio files\` or \`discord messages\` 
 
   - use \`!scp\` command to get scps, \`!scp random\` | \`!scp number_of_scp\`
@@ -15,6 +18,19 @@ const messages = {
   - use \`!site search\` to get a list of SCP foundations sites, \`use !site search site_number_as_listed\` to get a specified site
   
   - use \`!area\` to get a list of areas under the SCP foundation, use \`area !site number_as_listed\` to get a specified site and its description
+  
+  =============
+  MEMBER SYSTEM
+  =============
+
+  - every person gets 100 coupons
+  - every !scp command costs 5 coupons
+  - don't need to worry about registration, we will do it automatically for you
+  - if you run out of coupons, you need to wait a day and it will automatically refresh
+  - premium users get 500 coupons rather than 100
+  - voting our bot gives you 50 free coupons
+  - your coupons count and premium status persists over all servers that our bot services
+
   `,
   ready: [
     'Yes sir, retrieving archives from ████████',
